09780e2bd9e04bbea64f99e7187fa1d66e968962
CHRIS <chris@CHRISs-MBP.attlocal.net>
2022-09-07 22:28:30 -0700

    revising generating license

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  let licenseType = license.license; // 
-  let yourLicense = ''
-  if(licenseType === 'MIT') {
-    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
-  } else if (licenseType === 'GPLv3') {
-    yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`
-  } else if (licenseType === 'GPL') {
-    yourLicense = `![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)`
-  } else {
-    license.license = "N/A"
-  }
-  return yourLicense;
-};
-function generateMarkdown(data)
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
